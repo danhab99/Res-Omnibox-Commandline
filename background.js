a72cb05fa4df47be3ec46fc2870f42fb30fe9c16
Dan Habot <dan.habot@gmail.com>
2019-08-24 18:12:42 -0400

    Overhaul

--- background.js
+++ background.js
@@ -1,273 +1,138 @@
-var TRANSPARENT_MODE = false;
-var suggest_source;
 
 // This event is fired each time the user updates the text in the omnibox,
 // as long as the extension's keyword mode is still active.
 chrome.omnibox.onInputChanged.addListener(
 function(text, suggest)
 {
-	suggest_source = suggest;
-	Reccomend(prepare(text), suggest);
+	text = text.split('/')
+	if (text[0] == 'r') {
+		suggest('	Go to a specific subreddit')
+		return
+	}
+	if (text[0] == 'u') {
+		suggest('Go to a specific user')
+		return
+	}
+	if (text[0] == 'm' && text[1] != '') {
+		suggest('Go to your multireddit')
+		return
+	}
+	if (text[0] == 'm') {
+		suggest('Go to messages')
+		return
+	}
+	if (text[0] == 'mm') {
+		suggest('Go to modmail')
+		return
+	}
+
+	let isMe = text[0] == 'me'
+	let mecompare = param => text[1] == param[1] || text[1] == param
+	if (isMe && text[1] == 'sub' || text[1] == 'submissions') {
+		suggest('Go to your submissions')
+		return
+	}
+	if (isMe && mecompare('saved')) {
+		suggest('Go to your saves')
+		return
+	}
+	if (isMe && mecompare('comments')) {
+		suggest('Go to your comments')
+		return
+	}
+	if (isMe && mecompare('gilded')) {
+		suggest('Go to your gilded')
+		return
+	}
+	if (isMe && mecompare('likes')) {
+		suggest('Go to your likes')
+		return
+	}
+	if (isMe && mecompare('dislikes')) {
+		suggest('Go to your dislikes')
+		return
+	}
+	if (isMe && mecompare('hidden')) {
+		suggest('Go to posts you hid')
+		return
+	}
+
+	if (text[0] == 'post') {
+		suggest('Submit a new post')
+		return
+	}
+	if (text[0] == 'qm') {
+		suggest('Sends a quick message to a user')
+		return
+	}
+	if (text[0] == 'help') {
+		suggest('Go to the help page')
+		return
+	}
+	if (text[0] == 'discuss') {
+		suggest('Go to r/RESOmniLine')
+		return
+	}
+	if (text[0] == 'source') {
+		suggest("Go to the extension's source")
+		return
+	}
 });
+
+const openReddit = (endpoint, args) => {
+	// TODO: Implement args
+	let url = 'https://reddit.com/' + endpoint.join('/')
+	if (args) {
+		url += '?'
+		for (let key in args) {
+			url += key + '=' + args[key] + '&'
+		}
+	}
+	openPage(url)
+}
+
+const openPage = url => {
+	// TODO: Figure out how to do settings
+}
+
 // This event is fired with the user accepts the input in the omnibox.
 chrome.omnibox.onInputEntered.addListener(
 function(text)
 {
-	Do(prepare(text));
+	text = text.split('/')
+
+	if (text[0] == 'post') {
+		let sub = prompt('Enter subreddit name')
+		let title = prompt('Enter title')
+		let link = prompt('Enter a link')
+		openReddit(['r', sub, 'submit'], {
+			url: link,
+			title: title
+		})
+		return
+	}
+
+	if (text[0] == 'qm') {
+		let user = prompt('Enter recipients username')
+		let msg = prompt('Write out your message')
+		// TODO: Send message
+		return
+	}
+
+	if (text[0] == 'help') {
+		openPage('https://www.reddit.com/r/RESOmniLine/wiki/index')
+		return
+	}
+
+	if (text[0] == 'source') {
+		openPage('https://github.com/danhab99/Res-Omnibox-Commandline')
+		return
+	}
+
+	if (text[0] == 'discuss') {
+		openPage('https://www.reddit.com/r/RESOmniLine/')
+		return
+	}
+
+	openReddit(text)
 });
-
-
-
-//Reccomendations
-function Reccomend(text, suggest)
-{
-	console.log(text);
-	switch(text[0])
-	{
-		case "m":
-			//Open mail
-			recomend("Go to messages");
-			break;
-		case "mm":
-			//Open modmail
-			recomend("Go to modmail");
-			break;
-		case "me":
-			//Opperate on me
-			recomend("Go to your profile");
-			break;
-		case "r":
-			//Open subreddit
-			reccomend("Go to a subreddit", text[1]);
-			break;
-		case "u":
-			//Open user
-			if(text.length == 2)
-			{
-				reccomend("Go to a specific user", text[1]);
-			}
-			else if (text.length == 4)
-			{
-				reccomend("Go to a users multireddit", "user: " + text[1] + " multi: " + text[3]);
-			}
-			break;
-		case "help":
-			recomend("Open to help page");
-			break;
-		case "discuss":
-			recomend("Go to this extension's subreddit");
-			break;
-		case "f":
-		case "front":
-			reccomend("Go to front page");
-			break;
-		case "source":
-			reccomend("Go to source page on GitHub");
-			break;
-		case "qm":
-			reccomend("Send a quick message");
-			break;
-	}
-}
-
-//Do
-function Do(text)
-{
-	console.log(text);
-	switch(text[0])
-	{
-		case "m":
-			if(text.length >= 2)
-			{
-				//Open my multireddit
-				openMe("/m/" + text[1]);
-			}
-			else
-			{
-				openBlank("message/unread/");
-			}
-			break;
-		case "mm":
-			//Open modmail
-			openBlank("message/moderator/");
-			break;
-		case "me":
-			//Opperate on me
-			me(text);
-			break;
-		case "r":
-			//Open subreddit
-			openSub(text[1]);
-			break;
-		case "u":
-			//Open user
-			if(text.length == 2)
-			{
-				openUser(text[1]);
-			}
-			else if (text.length == 4)
-			{
-				openBlank("u/" + text[1] + "m/" + text[3]);
-			}
-			break;
-		case "help":
-			//alert("Not yet implimented");
-			openBlank("r/RESOmniLine/wiki/index");
-			break;
-		case "discuss":
-			openSub("RESOmniLine");
-			//openSub("OmniboxRES");
-			break;
-		case "f":
-		case "front":
-			openBlank("");
-			break;
-		case "source":
-			chrome.tabs.create({ url: "https://github.com/danhab99/Res-Omnibox-Commandline" });
-			break;
-		case "qm":
-			if (text.length  == 1) {
-				openBlank("message/compose/");
-			}
-			else if (text.length == 2){
-				//message/compose/?to=[USERNAME]&subject=[SUBJECT]
-				openBlank("message/compose/?to=" + text[1]);
-			}
-			else if (text.length == 3){
-				openBlank("message/compose/?to=" + text[1] + "&subject=" + text[2]);
-			}
-			break;
-		default:
-			var l = "Error: (" + text + ")";
-			alert(l);
-			console.log(l);
-			break;
-	}
-}
-
-function me(subs)
-{
-	var sw = subs.length;
-	switch(sw)
-	{
-		case 1:
-			openMe();
-			break;
-		case 2:
-			switch(subs[1])
-			{
-				case "s":
-				case "saved":
-					openMe("/saved");
-					break;
-				case "sub":
-				case "submitted":
-					openMe("/submitted");
-					break;
-				case "c":
-				case "comments":
-					openMe("/comments");
-					break;
-				case "g":
-				case "gilded":
-					openMe("/gilded");
-					break;
-				case "l":
-				case "liked":
-					openMe("/liked");
-					break;
-				case "d":
-				case "disliked":
-					openMe("/disliked");
-					break;
-				case "h":
-				case "hidden":
-					openMe("/hidden");
-					break;
-			}
-			break;
-		default:
-			alert("switch error: " + subs);
-			break;
-	}
-}
-
-//These components are for simplified use
-function prepare(text)
-{
-	if (!TRANSPARENT_MODE)
-	{
-		return text.split(/[ /]+/);
-	}
-	else
-	{
-		return text;
-	}
-}
-
-function openBlank(ext)
-{
-	chrome.tabs.create({ url: "http://www.reddit.com/" + ext });
-}
-
-function openUser(u)
-{
-	openBlank("user/" + u);
-}
-
-function openSub(r)
-{
-	openBlank("r/" + r);
-}
-
-function openMulti(u, m)
-{
-	openBlank("u/" + u + "/m/" + m);
-}
-
-function openMe(ext)
-{
-	if(ext != undefined)
-	{
-		openUser("me/" + ext);
-	}
-	else
-	{
-		openUser("me");
-	}
-}
-
-function recomend(c)
-{
-	reccomend(c,c);
-}
-
-function reccomend(c, d)
-{
-	//alert(c + d);
-	suggest_source([
-            { content: c, description: d }
-        ]);
-}
-
-function quickMessage(data)
-{
-	var to = data[1];
-	var body;
-	
-	for (var i = 0; i > data.length - 2; i++)
-	{
-		body = body + data[i] + "%20";
-	}
-	
-	openBlank("message/compose?to=" + to + "&subject=&message=" + body);
-}
-
-function post(data)
-{
-	var sub = data[1];
-	var title = data[2];
-	var url = data[3];
-	
-	openBlank("r/" + sub + "/submit?title=" + title + "&url=" + url);
-}
